538444f66dbbbb7ef49001073cd13e312651c12b
AndrewAscar742 <andrewmatosascar@outlook.com>
2023-03-02 22:33:45 -0300

    Build: Initial Commit

---
+++ src/main/java/br/com/senai/sollaris/ApiEmpresaPrestadorApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.sollaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmpresaPrestadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEmpresaPrestadorApplication.class, args);
+	}
+
+}
