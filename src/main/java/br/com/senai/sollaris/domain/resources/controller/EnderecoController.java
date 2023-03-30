7485cde17787dd1270a5b509b60f835237742986
AndrewAscar742 <andrewmatosascar@outlook.com>
2023-03-29 22:34:18 -0300

    Fix(Address Controller): CORS Resolved

--- src/main/java/br/com/senai/sollaris/domain/resources/controller/EnderecoController.java
+++ src/main/java/br/com/senai/sollaris/domain/resources/controller/EnderecoController.java
@@ -2,6 +2,7 @@ package br.com.senai.sollaris.domain.resources.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import br.com.senai.sollaris.domain.resources.service.EnderecoService;
 
 @RestController
 @RequestMapping("api/business/address")
+@CrossOrigin("http://localhost:3000")
 public class EnderecoController {
 	
 	@Autowired
