5bef8a587aee9af3d7245871c5d8ba0b5eef9bbb
Ddalmeida1 <eduardoalmeida4k@gmail.com>
2023-04-19 19:45:36 -0300

    Alteração CRUD Empresa

--- src/main/java/br/com/senai/sollaris/domain/resources/dtos/output/ReturnEmpresaPut.java
+++ src/main/java/br/com/senai/sollaris/domain/resources/dtos/output/ReturnEmpresaPut.java
@@ -11,21 +11,21 @@ import lombok.Setter;
 @RequiredArgsConstructor
 public class ReturnEmpresaPut {
 	
+	private Long id;
 	private String nome_fantasia;
 	private String razao_social;
 	private String cnpj;
 	private String telefone;
 	private String email;
 	private String senha;
-	private LocalDateTime dt_alteracao;
 	
 	public ReturnEmpresaPut(Empresa empresa) {
+		this.id = empresa.getId();
 		this.nome_fantasia = empresa.getNome_fantasia();
 		this.razao_social = empresa.getRazao_social();
 		this.cnpj = empresa.getCnpj();
 		this.telefone = empresa.getTelefone();
 		this.email = empresa.getEmail();
 		this.senha = empresa.getSenha();
-		this.dt_alteracao = empresa.getDt_alteracao();
 	}
 }
